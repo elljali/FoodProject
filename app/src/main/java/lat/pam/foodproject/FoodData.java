9565db70529fb3e2c326410e47cbedc86625233f
elljali <rizalsetiawan1210@gmail.com>
2021-11-05 23:20:34 +0700

    first commit

---
+++ app/src/main/java/lat/pam/foodproject/FoodData.java
@@ -0,0 +1,17 @@
+package lat.pam.foodproject;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+
+public class FoodData {
+    public static ArrayList<Food> getData(Context context) {
+        ArrayList<Food> list = new ArrayList<Food>();
+        list.add(new Food("Caphuchino", "Makananan Capuhcino", 5000, context.getDrawable(R.drawable.cappuchino)));
+        list.add(new Food("Cheesecake", "Makananan Cheesecake", 5000, context.getDrawable(R.drawable.cheesecake)));
+        list.add(new Food("Batagor", "Makananan batagor", 5000, context.getDrawable(R.drawable.batagor)));
+        list.add(new Food("Black Salad", "Makananan Salad", 5000, context.getDrawable(R.drawable.black_salad)));
+        return list;
+    }
+}
